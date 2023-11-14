1fed96e43c09f92fdf550ada1d69e0394b21bf4c
cabl0r <150492542+cabl0r@users.noreply.github.com>
2023-11-14 16:00:21 +0100

    add comments

--- pkg/model/ad.go
+++ pkg/model/ad.go
@@ -2,11 +2,11 @@ package model
 
 import "time"
 
-// Ad that is beeing stored
+// Ad represents an advertisement that is being stored in the database.
 type Ad struct {
-	ID        uint   `gorm:"primary_key"`
-	EbayID    string `gorm:"type:varchar(255)"`
-	QueryID   uint   `gorm:"index:ad_queryid"`
-	Location  string `gorm:"type:varchar(510)"`
-	CreatedAt time.Time
+	ID        uint      `gorm:"primary_key"`       // Primary key of the Ad in the database
+	EbayID    string    `gorm:"type:varchar(255)"` // Unique eBay ID for the ad, stored as a string
+	QueryID   uint      `gorm:"index:ad_queryid"`  // Foreign key referencing the associated query
+	Location  string    `gorm:"type:varchar(510)"` // Location information for the ad, stored as a string
+	CreatedAt time.Time // Timestamp representing when the ad was created
 }
